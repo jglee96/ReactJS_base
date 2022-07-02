d2ca56045bb1aa907494788dae90bf27d7995a79
jglee96 <zakklee96@gmail.com>
2022-07-03 01:01:23 +0900

    [feat] Make To Do List - part 1
    * To Do List 구현
    * format을 사용하여 input을 제어 컴포넌트로 사용

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  function byFn() {
-    console.log("bye :P");
-  }
-  function hiFn() {
-    console.log("created :)");
-    return byFn;
-  }
-
-  useEffect(hiFn, [])
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
