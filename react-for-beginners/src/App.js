2650af37834862db8fb8d2333b470c712868aa30
jglee96 <zakklee96@gmail.com>
2022-07-03 00:44:56 +0900

    [feat] useEffect cleanUP
    * component remove시에 함수를 실행시키는 방법
    * useEffect의 call back function에서 return하는 function을 component
    remove시에 실행함

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,27 +1,26 @@
 import { useEffect, useState } from "react";
 
+function Hello() {
+  function byFn() {
+    console.log("bye :P");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byFn;
+  }
+
+  useEffect(hiFn, [])
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue(prev => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when  'keyword' & 'counter' changes");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
