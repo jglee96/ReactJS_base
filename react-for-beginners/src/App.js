4425fd1b000642344dd1685863fc035fbd26af8a
jglee96 <zakklee96@gmail.com>
2022-07-03 17:49:19 +0900

    [feat] Coin Tracker
    * coinpaprika api를 사용하여 coin tracker 구현
    * input을 받아 현재 시세로 구매할 수 있는 coin 개수 보여주는 기능 구현

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,35 +1,53 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [cost, setCost] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const onChange = (event) => {
+    setCost(Number(event.target.value));
+  };
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
   };
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <>
+          <select onChange={onChange}>
+            {coins.map((coin, index) => (
+              <option
+                key={`${coin.name} - ${index}`}
+                value={coin.quotes.USD.price}
+              >
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          <form onSubmit={onSubmit}>
+            <input
+              onChange={(event) => setMoney(Number(event.target.value))}
+              value={money}
+              type="number"
+              placeholder="Enter your USD"
+            />
+          </form>
+        </>
+      )}
       <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      {cost !== 0 && money !== 0 ? <strong>{money / cost}</strong> : null}
     </div>
   );
 }
