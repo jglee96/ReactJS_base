c4f331838340b659953e5e49f550dc2ed07b8978
jglee96 <zakklee96@gmail.com>
2022-07-03 18:25:18 +0900

    [feat] Movie App part one
    * yts의 api를 사용하여 movie 정보를 가져옴
    * yts api는 cors 정책에 위반되기 때문에 그냥 가져올 수 없음 (보안이
    위험한 사이트인 듯)
    * 블로그(https://codemasterkimc.tistory.com/128) 참고하여 우회하여
    가져옴

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,52 +2,40 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [cost, setCost] = useState(0);
+  const [movies, setMoives] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://ancient-refuge-36430.herokuapp.com/https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMoives(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  const onChange = (event) => {
-    setCost(Number(event.target.value));
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-  };
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <>
-          <select onChange={onChange}>
-            {coins.map((coin, index) => (
-              <option
-                key={`${coin.name} - ${index}`}
-                value={coin.quotes.USD.price}
-              >
-                {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-              </option>
-            ))}
-          </select>
-          <form onSubmit={onSubmit}>
-            <input
-              onChange={(event) => setMoney(Number(event.target.value))}
-              value={money}
-              type="number"
-              placeholder="Enter your USD"
-            />
-          </form>
-        </>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       )}
-      <hr />
-      {cost !== 0 && money !== 0 ? <strong>{money / cost}</strong> : null}
     </div>
   );
 }
