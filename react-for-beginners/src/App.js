939618ea6d0df90aeaa08316152aaac5cd35620d
jglee96 <zakklee96@gmail.com>
2022-07-03 18:38:40 +0900

    [feat] Movie App part Two
    * react router setting
    * react router는 페이지 주소를 바라보고 있는 component

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,43 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMoives] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://ancient-refuge-36430.herokuapp.com/https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMoives(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
